4a75e2261afbe15854dc067347833c304a80b45b
Filpor <filip.porebinski@o2.pl>
2022-01-06 13:36:14 +0100

    add_print

--- lista_zakupow.py
+++ lista_zakupow.py
@@ -7,3 +7,4 @@ for key, value in shop_dictionary.items():
     print ("Idę do", key.capitalize() , "kupiuję", value)
 values_tab1=shop_dictionary.get("piekarnia")
 values_tab2=shop_dictionary.get("warzywniak")
+print("kupię", len(values_tab1+values_tab2) , "produktów")
