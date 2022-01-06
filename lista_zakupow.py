1c65188454ad80e629b0ce7e34a216ee7b7adebb
Filpor <filip.porebinski@o2.pl>
2022-01-06 13:53:23 +0100

    pozdrowienia_dla_mentora

--- lista_zakupow.py
+++ lista_zakupow.py
@@ -1,14 +1,1 @@
-print("Lista Zakupów")
-shop_dictionary= {
-    "piekarnia": ["chleb","bulki","paczek"],
-    "warzywniak": ["rukola","seler","marchew"]
-}
-for key, value in shop_dictionary.items():
-    print ("Idę do", key.capitalize() , "kupiuję", value)
-values_tab1=shop_dictionary.get("piekarnia")
-values_tab2=shop_dictionary.get("warzywniak")
-print("kupię", len(values_tab1+values_tab2) , "produktów")
-tab1=[1,2,3,4,5]
-tab2=["jeden","dwa","trzy"]
-print(tab1)
-print(tab2)
+print("pozdrowienia dla mentora")
