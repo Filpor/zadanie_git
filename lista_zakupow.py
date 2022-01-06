09ce3d4b0599ede287facd278be4903f82547218
Filpor <filip.porebinski@o2.pl>
2022-01-06 13:47:05 +0100

    add_tab1

--- lista_zakupow.py
+++ lista_zakupow.py
@@ -8,3 +8,5 @@ for key, value in shop_dictionary.items():
 values_tab1=shop_dictionary.get("piekarnia")
 values_tab2=shop_dictionary.get("warzywniak")
 print("kupię", len(values_tab1+values_tab2) , "produktów")
+tab1=[1,2,3,4,5]
+print(tab1)
